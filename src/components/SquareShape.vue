511a21cc2edc15df3cf5447a1b8c93086039ca70
Alexandre <alexandre.richard49410@gmail.com>
2022-02-25 02:37:10 +0100

    first commit

---
+++ src/components/SquareShape.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style=" { 'background-color' : color } "/>
+</template>
+
+<script>
+export default {
+    props: [
+        'color'
+    ]
+}
+</script>
+
+<style scoped>
+    div{
+      margin-right: 0.4rem;
+      display: inline-block;
+      height: 10rem;
+      width: 10rem;
+      box-shadow: 5px 5px 5px grey;
+      cursor: pointer;
+    }
+
+   
+
+</style>
